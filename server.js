35c36d9e0fdce89ae8b0cf18b68224982a11a6ef
Romanas <romanas.rodevicius@gmail.com>
2016-08-10 17:43:31 +0000

    Tests for jobs get and refactorired

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var jobModel = require('./models/Job');
+var jobsData = require('./jobs-data.js');
 
 var app = express();
 
@@ -7,9 +9,22 @@ app.set('view engine', 'jade');
 
 app.use(express.static(__dirname + '/public'));
 
+app.get('/api/jobs', function(req, res){
+   jobsData.findJobs().then(function(collection){
+       res.send(collection);
+   })
+});
+
 app.get('*', function(req, res){
     res.render('index');
 });
 
+
+//mongoose.connect('mongodb://localhost/jobfinder');
+jobsData.connectDB('mongodb://test:test@ds145395.mlab.com:45395/jobfinder-rr')
+.then(function(){
+   console.log("connected to mongodb successfully"); 
+   jobsData.seedJobs();
+});
 
 app.listen(process.env.PORT, process.env.IP);
